53c1b9a1c1fdb8f8f7a44f403240b04ba6461347
LorenzoBP <pmjlab@leeds.ac.uk>
2024-07-31 11:12:22 -0500

    first commit

---
+++ math_operations.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def substract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    # This function takes two numbers a and b and multiplies them and returns the result.
+    return a * b
+
+
+print(add(1, 2))
+print(substract(5, 3))
+print(multiply(2, 2))
